5d0f35a57e48a8223980d06dfad17ee3b6b5385e
dmitrikivilev <dimon_95.08@mail.ru>
2026-08-20 22:08:14 +0300

    cecar vol.2

--- python/quest/06 cecar.py
+++ python/quest/06 cecar.py
@@ -23,35 +23,64 @@ while True:
     else:
         print('Введите язык из перечня')
 while True:
-    user_input = input('Введите шаг сдвига для пароля: ')
-    if user_input.isdigit():
-        rot_N = int(user_input)
-        break
-    else:
-        print('Ошибка! Введите целое число.')
+    choise = input('р - если хотите использовать рендж н - если хотите использовать шаг ').lower()
+    if choise == 'р':
+        user_input = input('Введите рендж от 0 до ? ')
+        if user_input.isdigit():
+            rot_N = int(user_input)
+            break
+        else:
+            print('Ошибка! Введите целое число.')
+    if choise == 'н':
+        user_input = input('Введите шаг ? ')
+        if user_input.isdigit():
+            rot_N = int(user_input)
+            break
+        else:
+            print('Ошибка! Введите целое число. ')
 
 def decrypt(road,alph,n):
-    wrds = ""
-    txt = input("Введите слово/фразу: ")
-
-    if road == "д":
-        n = -n
-
-    for char in txt:
-        if char.lower() in alph:
-            index = alph.index(char.lower())
-            new_ind = (index + n) % len(alph)
-            new_char = alph[new_ind]
-            if char.isupper():
-                new_char = new_char.upper()
-            wrds += new_char
-        else:
-            wrds += char
-
+    if choise == "н":
+        wrds = ""
+        txt = input("Введите слово/фразу: ")
+        
+        if road == "д":
+            n = -n
             
-    return wrds
+        for char in txt:
+            if char.lower() in alph:
+                index = alph.index(char.lower())
+                new_ind = (index + n) % len(alph)
+                new_char = alph[new_ind]
+                if char.isupper():
+                    new_char = new_char.upper()
+                wrds += new_char
+            else:
+                wrds += char
 
+        return wrds 
+    
+    elif choise == "р":
+        txt = input("Введите слово/фразу: ")
 
+        for num in range(n, -1, -1):
+            wrds = ""
+            for char in txt:
+                if char.lower() in alph:
+                    index = alph.index(char.lower())
+                    if road == "д":
+                        new_ind = (index - num) % len(alph)  # ← ОШИБКА! num не существует здесь
+                    else:
+                        new_ind = (index + num) % len(alph)
+                    new_char = alph[new_ind]
+                    if char.isupper():
+                        new_char = new_char.upper()
+                    wrds += new_char
+                else:
+                    wrds += char
+            print (wrds)
 
-       
-print(decrypt(way,alphavit,rot_N))
+if choise == "р":
+    decrypt(way,alphavit,rot_N)
+else:
+    print(decrypt(way,alphavit,rot_N))
